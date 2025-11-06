430fa88c5933e6bf49ca7d6566d485cb5f237f39
Zavrka <ma.zavarzin@gmail.com>
2025-11-06 20:38:44 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,20 @@
+// next.config.js
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-
 import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withBundleAnalyzer = bundleAnalyzer({
-  // eslint-disable-next-line no-process-env
   enabled: process.env.ANALYZE === 'true'
 })
 
-export default withBundleAnalyzer({
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   staticPageGenerationTimeout: 300,
+  output: 'export',
+  reactStrictMode: true,
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'www.notion.so' },
@@ -21,14 +26,12 @@ export default withBundleAnalyzer({
     ],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true
   },
 
   webpack: (config) => {
-    // Workaround for ensuring that `react` and `react-dom` resolve correctly
-    // when using a locally-linked version of `react-notion-x`.
-    // @see https://github.com/vercel/next.js/issues/50391
-    const dirname = path.dirname(fileURLToPath(import.meta.url))
+    // Исправление путей для react и react-dom
     config.resolve.alias.react = path.resolve(dirname, 'node_modules/react')
     config.resolve.alias['react-dom'] = path.resolve(
       dirname,
@@ -37,6 +40,15 @@ export default withBundleAnalyzer({
     return config
   },
 
-  // See https://react-tweet.vercel.app/next#troubleshooting
+  experimental: {
+    esmExternals: 'loose'
+  },
+
+  env: {
+    NOTION_DISABLE_CACHE: 'true'
+  },
+
   transpilePackages: ['react-tweet']
-})
+}
+
+export default withBundleAnalyzer(nextConfig)
